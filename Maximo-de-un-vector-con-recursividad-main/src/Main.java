bb8e355a03630231f2c38098da7230ca1e37558e
MILTONSALAZAR2001 <87161156+MILTONSALAZAR2001@users.noreply.github.com>
2021-10-27 09:42:29 -0400

    Add files via upload

---
+++ Maximo-de-un-vector-con-recursividad-main/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //.out.println(Ejercicios.factorial(5));
+
+        //System.out.println(Ejercicios.mcd(412, 184));
+        //int[] vector = {4, 4, 6};
+        //System.out.println(Ejercicios.sumaVector(vector, 0));
+        //String palabra = "Hola";
+        //System.out.println(Ejercicios.invertirPalabra(palabra, palabra.length()-1));
+        //int[] vector1 = {5,6, 2};
+        //int[] vector2 = {5,6, 2};
+        //System.out.println(Ejercicios.compararVectores(vector1, vector2));
+        int [] vector = {2, 9, 6, 7, 6, 3, 4, 3};
+        System.out.println(Ejercicios.maximoVector(vector));
+    }
+}
